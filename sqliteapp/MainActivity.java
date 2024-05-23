99dea2ae06b292da50a6b6a40c69fbf00336bc3e
Mateus-0101 <61789289+Mateus-0101@users.noreply.github.com>
2024-05-22 21:46:11 -0300

    Update MainActivity.java

--- sqliteapp/MainActivity.java
+++ sqliteapp/MainActivity.java
@@ -1,12 +1,19 @@
 package iesb.app.sqliteapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+import iesb.app.sqliteapp.dao.DatabaseHandler;
+import iesb.app.sqliteapp.model.ClienteVO;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,4 +28,20 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    
+    public void btnOnClickCadastrarCliente(View view){
+        DatabaseHandler db = new DatabaseHandler(this);
+        Log.d("Insert: ", "Inserindo clientes...");
+        db.addCliente(new ClienteVO("Satan"));
+        db.addCliente(new ClienteVO("Hades"));
+        db.addCliente(new ClienteVO("Lula"));
+        
+        Log.d("Select: ", "Lendo os Clientes...");
+        List<ClienteVO> clientes = db.getAllClientes();
+        
+        for (ClienteVO cliente : clientes){
+            String log = "Id: " + cliente.getId();
+        }
+    }
+    
 }
